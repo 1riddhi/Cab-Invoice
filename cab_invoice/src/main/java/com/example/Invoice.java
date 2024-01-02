6857932ab8e479d4c9f9a4af407b444916578d7d
1riddhi <prajapatiriddhi074@gmail.com>
2024-01-02 06:24:11 +0530

    enhanced invoice

---
+++ cab_invoice/src/main/java/com/example/Invoice.java
@@ -0,0 +1,26 @@
+package main.java.com.example;
+
+public class Invoice {
+
+    private final int totalRides;
+    private final double totalFare;
+    private final double averageFarePerRide;
+
+    public Invoice(int totalRides, double totalFare, double averageFarePerRide) {
+        this.totalRides = totalRides;
+        this.totalFare = totalFare;
+        this.averageFarePerRide = averageFarePerRide;
+    }
+
+    public int getTotalRides() {
+        return totalRides;
+    }
+
+    public double getTotalFare() {
+        return totalFare;
+    }
+
+    public double getAverageFarePerRide() {
+        return averageFarePerRide;
+    }
+}
