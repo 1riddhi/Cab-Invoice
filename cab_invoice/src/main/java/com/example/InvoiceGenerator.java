5772402937349ceda1b1c70ca6cb606d5e4cc2c0
1riddhi <prajapatiriddhi074@gmail.com>
2024-01-02 06:19:14 +0530

    calculateTotalFare

--- cab_invoice/src/main/java/com/example/InvoiceGenerator.java
+++ cab_invoice/src/main/java/com/example/InvoiceGenerator.java
@@ -1,5 +1,7 @@
 package main.java.com.example;
 
+import java.util.List;
+
 public class InvoiceGenerator {
     private static final double COST_PER_KM = 10.0;
     private static final double COST_PER_MINUTE = 1.0;
@@ -9,4 +11,13 @@ public class InvoiceGenerator {
         double fare = distance * COST_PER_KM + time * COST_PER_MINUTE;
         return Math.max(fare, MINIMUM_FARE);
     }
+
+    public double calculateTotalFare(List<Ride> rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+    }
+    
 }
