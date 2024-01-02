0f0cb31c57a10cfbca5183eb92c0554893c69dd6
1riddhi <prajapatiriddhi074@gmail.com>
2024-01-02 06:14:58 +0530

    ride

---
+++ cab_invoice/src/main/java/com/example/Ride.java
@@ -0,0 +1,20 @@
+package main.java.com.example;
+
+public class Ride {
+    
+    private final double distance;
+    private final int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
